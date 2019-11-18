654772a0688ac610bf2e795fcc2060c67c974189
Jrodan88 <jayoso88@gmail.com>
2019-11-18 15:16:45 -0700

    mapped state to props in results posts component and rendered each post with our post component, and implemented home link in logo component

--- src/components/post.js
+++ src/components/post.js
@@ -3,11 +3,10 @@ import React, { Component } from "react";
 class Post extends Component {
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
-      return (
-        <span className="post-topic" key={index}>
-          {topic}
-        </span>
-      );
+      return;
+      <span className="post-topic" key={index}>
+        {topic}
+      </span>;
     });
     return topics;
   }
