15e0f05e00918c0fea68638b3d64f3c7fcd39391
Jrodan88 <jayoso88@gmail.com>
2019-11-25 13:24:55 -0700

    finished JS substrings for post links

--- src/components/post.js
+++ src/components/post.js
@@ -20,13 +20,33 @@ class Post extends Component {
     return topics;
   }
 
+  getNameForPostLink(str) {
+    var n = str.lastIndexOf("/");
+    var link = str.substring(n + 1, str.length);
+    if (n + 1 == str.length) {
+      link = str.slice(0, n);
+      n = link.lastIndexOf("/");
+      link = str.substring(n + 1, str.length - 1);
+    }
+
+    if (link.includes(".html")) {
+      link = link.substring(0, link.length - 5);
+    }
+    if (link.includes(".htm")) {
+      link = link.substring(0, link.length - 4);
+    }
+    return link;
+  }
+
   renderLinks() {
     let links = this.props.post_links.map((post_link, index) => {
       return (
         <div className="post-link" key={index}>
           <div className="post-link__box"></div>
           <div className="post-link__link">
-            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+            <a href={post_link.link_url}>
+              {this.getNameForPostLink(post_link.link_url)}
+            </a>
           </div>
         </div>
       );
@@ -38,7 +58,9 @@ class Post extends Component {
     if (this.props.type == "recent") {
       return (
         <li className="recent-post">
-          <div className="recent-post__title">{this.props.title}</div>
+          <div className="recent-post__title">
+            <a href={this.props.url_for_post}>{this.props.title}</a>
+          </div>
           <div className="recent-post__topics">{this.renderTopics()}</div>
         </li>
       );
